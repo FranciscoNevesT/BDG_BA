ac063a800633014dfbfd0258dd23b5207da08e17
nekoraw <michelbarros123456@gmail.com>
2025-01-08 21:26:34 -0300

    fix: corrige downloads incompletos e erros na criação do sql (e formatação)

--- data_to_sql/main.py
+++ data_to_sql/main.py
@@ -4,18 +4,22 @@ import numpy as np
 import pandas as pd
 import geopandas
 
+
 def remove_aspas(value):
     """Removes double quotes from a string."""
     if isinstance(value, str):
-        return value.replace('"', '')
+        return value.replace('"', "")
     return value
 
 
 def read_csv(path):
-
     """Reads a CSV file, removes inconsistent rows, and drops columns with only one unique value."""
-    with open(path,"r") as file:
-        text = file.read()
+    try:
+        with open(path, "r") as file:
+            text = file.read()
+    except UnicodeDecodeError:
+        with open(path, "rb") as file:
+            text = file.read().decode("latin1")
 
     num_campos = len(text.split("\n")[0].split(";"))
     data = []
@@ -30,7 +34,7 @@ def read_csv(path):
     data = pd.DataFrame(data, columns=columns)
 
     # Corrigir strings
-    data = data.applymap(remove_aspas)
+    data = data.map(remove_aspas)
 
     for col in data.columns:
         if data[col].nunique() == 1:
@@ -42,178 +46,249 @@ def read_csv(path):
         except:
             try:
                 column_values = data[i]
-                column_values = column_values.apply(lambda x: x.replace(",", ".")).astype(float)
+                column_values = column_values.apply(
+                    lambda x: x.replace(",", ".")
+                ).astype(float)
                 data[i] = column_values
             except:
                 pass
 
     return data
 
+
 def corrigir_municipos_nomes(data):
     """Standardizes municipality names."""
     corrections = {
-        'CAMACÃ': 'CAMACAN',
-        'SANTO ESTEVÃO': 'SANTO ESTÊVÃO',
-        'CAEM': 'CAÉM',
-        'DIAS D ÁVILA': "DIAS D'ÁVILA",
-        'Araças':'ARAÇÁS',
-        'Iuiú': 'IUIU',
-        'Santa Teresinha': 'SANTA TEREZINHA',
-        'Muquém de São Francisco': 'MUQUÉM DO SÃO FRANCISCO'
-
+        "CAMACÃ": "CAMACAN",
+        "SANTO ESTEVÃO": "SANTO ESTÊVÃO",
+        "CAEM": "CAÉM",
+        "DIAS D ÁVILA": "DIAS D'ÁVILA",
+        "Araças": "ARAÇÁS",
+        "Iuiú": "IUIU",
+        "Santa Teresinha": "SANTA TEREZINHA",
+        "Muquém de São Francisco": "MUQUÉM DO SÃO FRANCISCO",
     }
     return data.replace(corrections)
 
+
 def create_table(dataframe, table_name, connection):
     """Creates a table in the SQLite database from a DataFrame."""
-    dataframe.to_sql(table_name, connection, if_exists='replace', index=False)
+    dataframe.to_sql(table_name, connection, if_exists="replace", index=False)
+
 
 def format_municipios(db_file):
     """Loads and formats municipality data."""
     data = geopandas.read_file("datasets/municipios/BA_Municipios_2022.dbf")
 
     ## municipio
-    data_sql = data.drop(['NM_MUN', 'SIGLA_UF'], axis=1)
-    data_sql.to_file(db_file, driver='SQLite', layer="municipio")
+    data_sql = data.drop(["NM_MUN", "SIGLA_UF"], axis=1)
+    data_sql.to_file(db_file, driver="SQLite", layer="municipio")
 
     return data
 
-def format_partido(connection,mun):
-    #Load data
+
+def format_partido(connection, mun):
+    # Load data
     data = read_csv("datasets/partido/votacao_partido_munzona_2022_BA.csv")
 
     # Handle missing values
-    data.replace(data['SG_FEDERACAO'].iloc[0], pd.NA, inplace=True)
+    data.replace(data["SG_FEDERACAO"].iloc[0], pd.NA, inplace=True)
 
     # Drop redundant columns
-    redundant_cols = ['CD_ELEICAO', 'DT_ELEICAO', 'NR_FEDERACAO', 'CD_CARGO']
+    redundant_cols = ["CD_ELEICAO", "DT_ELEICAO", "NR_FEDERACAO", "CD_CARGO"]
     data.drop(columns=redundant_cols, inplace=True)
 
-
     # Corrigir municipios nomes
     data = corrigir_municipos_nomes(data)
-    mun_dict = {nm_mun.upper(): cd_mun for cd_mun, nm_mun in mun[['CD_MUN', 'NM_MUN']].values}
-    data['CD_MUN'] = data['NM_MUNICIPIO'].apply(lambda x: mun_dict[x])
-    data['NM_MUN'] = data['NM_MUNICIPIO']
+    mun_dict = {
+        nm_mun.upper(): cd_mun for cd_mun, nm_mun in mun[["CD_MUN", "NM_MUN"]].values
+    }
+    data["CD_MUN"] = data["NM_MUNICIPIO"].apply(lambda x: mun_dict[x])
+    data["NM_MUN"] = data["NM_MUNICIPIO"]
 
-    data['CD_MUN'] = data['NM_MUNICIPIO'].apply(lambda x: mun_dict[x])
-    data['NM_MUN'] = data['NM_MUNICIPIO']
+    data["CD_MUN"] = data["NM_MUNICIPIO"].apply(lambda x: mun_dict[x])
+    data["NM_MUN"] = data["NM_MUNICIPIO"]
 
-    data.drop(['CD_MUNICIPIO', 'NM_MUNICIPIO'], axis=1, inplace=True)
+    data.drop(["CD_MUNICIPIO", "NM_MUNICIPIO"], axis=1, inplace=True)
 
     # Create the sql tables
-    create_table(data[[
-        'CD_MUN', 'NR_TURNO', 'NR_ZONA', 'DS_CARGO', 'NR_PARTIDO',
-        'QT_VOTOS_LEGENDA_VALIDOS', 'QT_TOTAL_VOTOS_LEG_VALIDOS',
-        'QT_VOTOS_NOMINAIS_VALIDOS', 'SQ_COLIGACAO'
-    ]], "votos_partido", connection)
+    create_table(
+        data[
+            [
+                "CD_MUN",
+                "NR_TURNO",
+                "NR_ZONA",
+                "DS_CARGO",
+                "NR_PARTIDO",
+                "QT_VOTOS_LEGENDA_VALIDOS",
+                "QT_TOTAL_VOTOS_LEG_VALIDOS",
+                "QT_VOTOS_NOMINAIS_VALIDOS",
+                "SQ_COLIGACAO",
+            ]
+        ],
+        "votos_partido",
+        connection,
+    )
 
-    create_table(data[[
-        'NR_PARTIDO', 'NM_PARTIDO', 'TP_AGREMIACAO', 'SG_PARTIDO'
-    ]].drop_duplicates().dropna(), "partido", connection)
+    create_table(
+        data[["NR_PARTIDO", "NM_PARTIDO", "TP_AGREMIACAO", "SG_PARTIDO"]]
+        .drop_duplicates()
+        .dropna(),
+        "partido",
+        connection,
+    )
 
-    create_table(data[[
-        'SG_FEDERACAO', 'NM_FEDERACAO', 'DS_COMPOSICAO_FEDERACAO'
-    ]].drop_duplicates().dropna(), "federacao", connection)
+    create_table(
+        data[["SG_FEDERACAO", "NM_FEDERACAO", "DS_COMPOSICAO_FEDERACAO"]]
+        .drop_duplicates()
+        .dropna(),
+        "federacao",
+        connection,
+    )
 
-    create_table(data[[
-        'NM_COLIGACAO', 'SQ_COLIGACAO', 'DS_COMPOSICAO_COLIGACAO'
-    ]].drop_duplicates().dropna(), "coligacao", connection)
+    create_table(
+        data[["NM_COLIGACAO", "SQ_COLIGACAO", "DS_COMPOSICAO_COLIGACAO"]]
+        .drop_duplicates()
+        .dropna(),
+        "coligacao",
+        connection,
+    )
 
-    create_table(data[[
-        'CD_MUN', 'NM_MUN'
-    ]].drop_duplicates().dropna(), "municipio_nome", connection)
+    create_table(
+        data[["CD_MUN", "NM_MUN"]].drop_duplicates().dropna(),
+        "municipio_nome",
+        connection,
+    )
 
-def format_candidato(connection,mun):
+
+def format_candidato(connection, mun):
     """Processes and formats candidate data."""
-    #Load data
+    # Load data
     data = read_csv("datasets/candidato/votacao_candidato_munzona_2022_BA.csv")
 
+    data.rename(columns={'DS_SIT_TOT_TURNO"': "DS_SIT_TOT_TURNO"}, inplace=True)
+
     # Handle missing values
-    data.replace(data['SG_FEDERACAO'].iloc[0], pd.NA, inplace=True)
+    data.replace(data["SG_FEDERACAO"].iloc[0], pd.NA, inplace=True)
 
     # Tirar dados redundantes
-    redudante = ['CD_ELEICAO', 'DT_ELEICAO', 'NR_FEDERACAO', 'CD_CARGO']
+    redudante = ["CD_ELEICAO", "DT_ELEICAO", "NR_FEDERACAO", "CD_CARGO"]
     data.drop(redudante, axis=1, inplace=True)
-
 
     # Corrigir municipios nomes
     data = corrigir_municipos_nomes(data)
-    mun_dict = {nm_mun.upper(): cd_mun for cd_mun, nm_mun in mun[['CD_MUN', 'NM_MUN']].values}
-    data['CD_MUN'] = data['NM_MUNICIPIO'].apply(lambda x: mun_dict[x])
-    data['NM_MUN'] = data['NM_MUNICIPIO']
+    mun_dict = {
+        nm_mun.upper(): cd_mun for cd_mun, nm_mun in mun[["CD_MUN", "NM_MUN"]].values
+    }
+    data["CD_MUN"] = data["NM_MUNICIPIO"].apply(lambda x: mun_dict[x])
+    data["NM_MUN"] = data["NM_MUNICIPIO"]
 
-    data['CD_MUN'] = data['NM_MUNICIPIO'].apply(lambda x: mun_dict[x])
-    data['NM_MUN'] = data['NM_MUNICIPIO']
+    data["CD_MUN"] = data["NM_MUNICIPIO"].apply(lambda x: mun_dict[x])
+    data["NM_MUN"] = data["NM_MUNICIPIO"]
 
-    data.drop(['CD_MUNICIPIO', 'NM_MUNICIPIO'], axis=1, inplace=True)
+    data.drop(["CD_MUNICIPIO", "NM_MUNICIPIO"], axis=1, inplace=True)
 
     # Create the sql tables
-    create_table(data[[
-        'NR_TURNO', 'NR_ZONA', 'DS_CARGO', 'NR_CANDIDATO', 'NM_CANDIDATO',
-        'NM_URNA_CANDIDATO', 'NM_SOCIAL_CANDIDATO', 'CD_SITUACAO_CANDIDATURA',
-        'DS_SITUACAO_CANDIDATURA', 'NR_PARTIDO', 'SG_FEDERACAO', 'SQ_COLIGACAO',
-        'QT_VOTOS_NOMINAIS', 'NM_TIPO_DESTINACAO_VOTOS', 'QT_VOTOS_NOMINAIS_VALIDOS',
-        'CD_SIT_TOT_TURNO', 'DS_SIT_TOT_TURNO', 'CD_MUN'
-    ]], "votos_candidato", connection)
+    create_table(
+        data[
+            [
+                "NR_TURNO",
+                "NR_ZONA",
+                "DS_CARGO",
+                "NR_CANDIDATO",
+                "NM_CANDIDATO",
+                "NM_URNA_CANDIDATO",
+                "NM_SOCIAL_CANDIDATO",
+                "CD_SITUACAO_CANDIDATURA",
+                "DS_SITUACAO_CANDIDATURA",
+                "NR_PARTIDO",
+                "SG_FEDERACAO",
+                "SQ_COLIGACAO",
+                "QT_VOTOS_NOMINAIS",
+                "NM_TIPO_DESTINACAO_VOTOS",
+                "QT_VOTOS_NOMINAIS_VALIDOS",
+                "CD_SIT_TOT_TURNO",
+                "DS_SIT_TOT_TURNO",
+                "CD_MUN",
+            ]
+        ],
+        "votos_candidato",
+        connection,
+    )
+
 
 def format_censo(connection):
-    csv_names = ['alfabetizacao', 'basico', 'domicilio1', 'domicilio2', 'domicilio3', 'cor_raca', 'demografia',
-                 'domicilio_indigena', 'domicilio_quilombolas', 'obitos', 'parentesco', 'indigenas',
-                 'quilombolas']
+    csv_names = [
+        "alfabetizacao",
+        "basico",
+        "domicilio1",
+        "domicilio2",
+        "domicilio3",
+        "cor_raca",
+        "demografia",
+        "domicilio_indigena",
+        "domicilio_quilombolas",
+        "obitos",
+        "parentesco",
+        "indigenas",
+        "quilombolas",
+    ]
 
     for csv in csv_names:
         # Load data
         path = "datasets/{}/".format(csv)
-        csv_path = "{}{}".format(path,os.listdir(path)[0])
+        csv_path = "{}{}".format(path, os.listdir(path)[0])
 
         data = read_csv(csv_path)
 
-        data.drop('NM_MUN',axis=1,inplace=True)
+        data.drop("NM_MUN", axis=1, inplace=True)
 
-        data.to_sql(csv, connection, if_exists='replace', index=False)
+        data.to_sql(csv, connection, if_exists="replace", index=False)
+
 
 def format_cad_unico(connection):
     cad_unico = pd.read_csv("datasets/cad_unico/cad_unico.csv")
 
-    cad_unico['anomes'] = cad_unico['anomes'].astype(str)
-    cad_unico['ano'] = cad_unico['anomes'].str[:4]
-    cad_unico['mes'] = cad_unico['anomes'].str[4:]
-    cad_unico['ano'] = cad_unico['ano'].astype(int)
-    cad_unico['mes'] = cad_unico['mes'].astype(int)
-    cad_unico.drop('anomes', axis=1, inplace=True)
+    cad_unico["anomes"] = cad_unico["anomes"].astype(str)
+    cad_unico["ano"] = cad_unico["anomes"].str[:4]
+    cad_unico["mes"] = cad_unico["anomes"].str[4:]
+    cad_unico["ano"] = cad_unico["ano"].astype(int)
+    cad_unico["mes"] = cad_unico["mes"].astype(int)
+    cad_unico.drop("anomes", axis=1, inplace=True)
 
-    cad_unico['CD_MUN'] = cad_unico['ibge']
-    cad_unico.drop('ibge',axis=1,inplace=True)
+    cad_unico["CD_MUN"] = cad_unico["ibge"]
+    cad_unico.drop("ibge", axis=1, inplace=True)
 
-    table_name = 'cadastro_unico'
-    cad_unico.to_sql(table_name, connection, if_exists='replace', index=False)
+    table_name = "cadastro_unico"
+    cad_unico.to_sql(table_name, connection, if_exists="replace", index=False)
+
 
 def format_bolsa_familia(connection):
     bf_2021 = pd.read_csv("datasets/bolsa_familia/bolsa_familia.csv")
 
-    bf_2021['anomes'] = bf_2021['anomes'].astype(str)
-    bf_2021['ano'] = bf_2021['anomes'].str[:4]
-    bf_2021['mes'] = bf_2021['anomes'].str[4:]
-    bf_2021['ano'] = bf_2021['ano'].astype(int)
-    bf_2021['mes'] = bf_2021['mes'].astype(int)
-    bf_2021.drop('anomes', axis=1, inplace=True)
+    bf_2021["anomes"] = bf_2021["anomes"].astype(str)
+    bf_2021["ano"] = bf_2021["anomes"].str[:4]
+    bf_2021["mes"] = bf_2021["anomes"].str[4:]
+    bf_2021["ano"] = bf_2021["ano"].astype(int)
+    bf_2021["mes"] = bf_2021["mes"].astype(int)
+    bf_2021.drop("anomes", axis=1, inplace=True)
 
-    bf_2021.dropna(axis=0,inplace=True)
+    bf_2021.dropna(axis=0, inplace=True)
 
-    bf_2021['CD_MUN'] = bf_2021['ibge']
-    bf_2021.drop('ibge',axis=1,inplace=True)
+    bf_2021["CD_MUN"] = bf_2021["ibge"]
+    bf_2021.drop("ibge", axis=1, inplace=True)
 
-    table_name = 'bolsa_familia'
-    bf_2021.to_sql(table_name, connection, if_exists='replace', index=False)
+    table_name = "bolsa_familia"
+    bf_2021.to_sql(table_name, connection, if_exists="replace", index=False)
+
 
 def format_censo_agro(connection, mun):
-    gdf = geopandas.read_file('datasets/censo_agro/mun_agro/mun_agro.shp')
+    gdf = geopandas.read_file("datasets/censo_agro/mun_agro/mun_agro.shp")
 
-    gdf[['municipio', 'estado']] = gdf['MUNICIPIO'].str.split(' - ', expand=True)
+    gdf[["municipio", "estado"]] = gdf["MUNICIPIO"].str.split(" - ", expand=True)
 
-    gdf = gdf[gdf['estado'] == 'BA']
-    gdf.drop(['estado', 'MUNICIPIO'], axis=1, inplace=True)
+    gdf = gdf[gdf["estado"] == "BA"]
+    gdf.drop(["estado", "MUNICIPIO"], axis=1, inplace=True)
 
     rename_dict = {
         "V1": "num_estab_mun",  # Total de estabelecimentos agropecuários, por município.
@@ -298,36 +373,45 @@ def format_censo_agro(connection, mun):
 
     mun_index = {i[1].lower(): int(i[0]) for i in mun.values}
 
-    gdf['CD_MUN'] = gdf['municipio'].apply(lambda x: mun_index[x.lower()])
+    gdf["CD_MUN"] = gdf["municipio"].apply(lambda x: mun_index[x.lower()])
 
-    gdf.drop(['OBJECTID','Shape_Leng','Shape_Area','geometry','municipio'],axis=1,inplace=True)
+    gdf.drop(
+        ["OBJECTID", "Shape_Leng", "Shape_Area", "geometry", "municipio"],
+        axis=1,
+        inplace=True,
+    )
     gdf = gdf.reset_index(drop=True)
 
-    table_name = 'censo_agro'
-    gdf.to_sql(table_name, connection, if_exists='replace', index=False)
+    table_name = "censo_agro"
+    gdf.to_sql(table_name, connection, if_exists="replace", index=False)
 
-def create_db(db_path = 'sql'):
-    name = 'eleicao.db'
+
+def create_db(db_path="sql"):
+    name = "eleicao.db"
     db_file = os.path.join(db_path, name)
+
+    if not os.path.exists(db_path):
+        os.mkdir(db_path)
 
     if os.path.isfile(db_file):
         os.remove(db_file)
+    else:
+        open(db_file, "w").close()
 
     mun = format_municipios(db_file)
 
     connection = sqlite3.connect(db_file)
 
-    format_partido(connection = connection, mun = mun)
-    format_candidato(connection = connection, mun = mun)
-    format_censo(connection = connection)
+    format_partido(connection=connection, mun=mun)
+    format_candidato(connection=connection, mun=mun)
+    format_censo(connection=connection)
     format_cad_unico(connection=connection)
     format_bolsa_familia(connection=connection)
-    format_censo_agro(connection=connection,mun = mun)
+    format_censo_agro(connection=connection, mun=mun)
 
     connection.close()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     create_db()
